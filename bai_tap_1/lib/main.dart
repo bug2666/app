d9f504481aa962575e5d3dd5541e3cf90cea632c
bug2666 <vanloa402@gmail.com>
2025-12-14 22:02:38 +0700

    Add bai_tap_1 Flutter project

---
+++ bai_tap_1/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bai_tap_1/ClassRoom.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Classroom(),
+    );
+  }
+}
+
